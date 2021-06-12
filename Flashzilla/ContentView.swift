ecd0d07acb749afa91d21d86bd926b2bb1971db7
Brandon Knox <bknox.dev@gmail.com>
2021-06-12 11:59:58 -0700

    Adding counter to timer to quit after 5 runs

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
     
     var body: some View {
         Text("Hello, World!")
             .onReceive(timer) { time in
-                print("The time is now \(time)")
+                if self.counter == 5 {
+                    self.timer.upstream.connect().cancel()
+                } else {
+                    print("The time is now \(time)")
+                }
+                
+                self.counter += 1
             }
     }
 }
