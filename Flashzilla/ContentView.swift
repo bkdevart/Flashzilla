1fe1b393a19aae84465ac32486de336c754be2b5
Brandon Knox <bknox.dev@gmail.com>
2021-06-11 18:17:43 -0700

    turning off hit testing on circle to make underlying square receive taps

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                 .onTapGesture {
                     print("Circle tapped!")
                 }
+                .allowsHitTesting(false)
         }
     }
 }
