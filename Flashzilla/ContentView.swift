c6f50d945f7d8eaa631f6b0cf09c625c6dd8a2e3
Brandon Knox <bknox.dev@gmail.com>
2021-06-11 17:56:32 -0700

    using simple haptics - success

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,34 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in self.offset = value.translation }
-            .onEnded { _ in
-                withAnimation {
-                    self.offset = .zero
-                    self.isDragging = false
-                }
-            }
-        
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-            }
-        
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        return Circle()
-            .fill(Color.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        Text("Hello, World!")
+            .onTapGesture(perform: simpleSuccess)
+    }
+    
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 }
 
