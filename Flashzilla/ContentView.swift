169c54cc18b10b21b518b00e026a7be171834c71
Brandon Knox <bknox.dev@gmail.com>
2021-06-11 18:05:17 -0700

    More complex haptics implemented with CoreHaptics.  prepareHaptics() created to start engine, and complexSuccess used to perform haptics

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -5,18 +5,53 @@
 //  Created by Brandon Knox on 6/11/21.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
         Text("Hello, World!")
-            .onTapGesture(perform: simpleSuccess)
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
     }
     
     func simpleSuccess() {
         let generator = UINotificationFeedbackGenerator()
         generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware()
+                .supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard
+            CHHapticEngine.capabilitiesForHardware()
+                .supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play patttern: \(error.localizedDescription).")
+        }
     }
 }
 
