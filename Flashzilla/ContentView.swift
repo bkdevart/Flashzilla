f9d4629bcdff66b4c165f50afc2d5719e4777787
Brandon Knox <bknox.dev@gmail.com>
2021-06-12 12:20:02 -0700

    Detecting when application moves to the background

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,19 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
+
     
     var body: some View {
         Text("Hello, World!")
-            .onReceive(timer) { time in
-                if self.counter == 5 {
-                    self.timer.upstream.connect().cancel()
-                } else {
-                    print("The time is now \(time)")
-                }
-                
-                self.counter += 1
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                print("Moving to the background!")
             }
     }
 }
