a413f4a262fab7fda250183b9f5bc9f3b220a680
Brandon Knox <bknox.dev@gmail.com>
2021-06-13 12:13:31 -0700

    Created Card struct and CardView, along with variables to toggle state between showing and hiding answers.

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,23 +7,10 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
     
     var body: some View {
-        Text("Hello, World!")
-            .padding()
-            .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
-            .foregroundColor(Color.white)
-            .clipShape(Circle())
+        CardView(card: Card.example)
     }
 }
 
