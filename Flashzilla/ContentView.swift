9bbf16e4e3531b4ccaa68b0246e7f597c52f0d24
Brandon Knox <bknox.dev@gmail.com>
2021-06-14 05:15:12 -0700

    Added timer to count down from 100, and isActive variable to track state of application to pause timer when application is exited

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -18,13 +18,28 @@ struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
+    @State private var isActive = true
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         ZStack {
             Image("background")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
+            
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -47,7 +62,7 @@ struct ContentView: View {
                             .background(Color.black.opacity(0.7))
                             .clipShape(Circle())
                         Spacer()
-                        Image(systemName: "Checkmark.circle")
+                        Image(systemName: "checkmark.circle")
                             .padding()
                             .background(Color.black.opacity(0.7))
                             .clipShape(Circle())
@@ -57,6 +72,16 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else { return }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in self.isActive = true
         }
     }
     
